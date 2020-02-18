575cd02433e7ea928ebf76be6d6713d2571642ab
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-02-18 22:37:15 +0200

    variables in create_user mutation

--- schema.py
+++ schema.py
@@ -44,8 +44,8 @@ schema = graphene.Schema(query=Query, mutation=Mutation)
 
 result = schema.execute(
     '''
-    mutation {
-        createUser(username: "Jeff") {
+    mutation ($username: String) {
+        createUser(username: $username) {
             user {
                 id
                 username
@@ -53,7 +53,8 @@ result = schema.execute(
             }
         }
     }
-    '''
+    ''',
+    variable_values={'username': 'Dave'}
 )
 
 print(result.errors)
