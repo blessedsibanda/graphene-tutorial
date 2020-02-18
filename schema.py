8987a2538990c9879296fd80fcb60744ade87544
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-02-18 22:18:04 +0200

    users query

--- schema.py
+++ schema.py
@@ -1,9 +1,16 @@
 import graphene
 import json
+from datetime import datetime
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    created_at = graphene.DateTime()
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
     is_admin = graphene.Boolean()
+    users = graphene.List(User)
 
     def resolve_hello(self, info):
         return 'world'
@@ -11,12 +18,24 @@ class Query(graphene.ObjectType):
     def resolve_is_admin(self, info):
         return True 
 
+    def resolve_users(self, info):
+        return [
+            User(id="1", username="Fred", created_at=datetime.now()),
+            User(id="2", username="Jane", created_at=datetime.now()),
+            User(id="3", username="Samantha", created_at=datetime.now()),
+            User(id="4", username="Blessed", created_at=datetime.now()),
+        ]
+
 schema = graphene.Schema(query=Query, auto_camelcase=False)
 
 result = schema.execute(
     '''
     {
-        is_admin
+        users {
+            username
+            id
+            created_at
+        }
     }
     '''
 )
