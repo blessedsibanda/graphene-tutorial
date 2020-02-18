aef66eea81607615def38ae315c4ed98b9a6e47b
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-02-18 22:13:11 +0200

    camel case for graphql operations

--- schema.py
+++ schema.py
@@ -3,16 +3,20 @@ import json
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    is_admin = graphene.Boolean()
 
     def resolve_hello(self, info):
         return 'world'
+
+    def resolve_is_admin(self, info):
+        return True 
 
 schema = graphene.Schema(query=Query)
 
 result = schema.execute(
     '''
     {
-        hello
+        isAdmin
     }
     '''
 )
@@ -20,8 +24,6 @@ result = schema.execute(
 print(result.data.items())
 
 print(result.data)
-print(result.data['hello'])
-
 
 # convert result to json
 dictResult = dict(result.data.items())
